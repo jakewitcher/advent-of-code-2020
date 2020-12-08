3fbb2bb63a72ae5de14560a3c81e0fb0ce9e3e66
Jake Witcher <jjwitcher09@gmail.com>
2020-12-07 23:12:10 -0500

    init commit for day 8

---
+++ day-8/internal/input/input.go
@@ -0,0 +1,19 @@
+package input
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func Extract() ([]string, error) {
+	file, err := ioutil.ReadFile("internal/input/input.txt")
+	if err != nil {
+		log.Println("failed to read file")
+		return nil, err
+	}
+
+	input := strings.Split(string(file), "\r\n")
+
+	return input, nil
+}
